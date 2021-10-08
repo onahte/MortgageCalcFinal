eb20ae31596973b87fd5a1aa630ced6b79f56254
onahte <76444607+onahte@users.noreply.github.com>
2021-10-07 20:03:46 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,8 +3,17 @@ package MortgageCalcFinal
 public class Main {
 
     public static void main(String[] args) {
-        var mortgage = new Mortgage();
-        mortgage.printMonthlyPayment();
-        mortgage.printPaymentSchedule();
+
+        int principal = (int) Console.readNumber("How much would you like to loan? ",
+                1000, 1_000_000);
+        float rate = (float) Console.readNumber("What is the annual loan rate? ",
+                0, 20);
+        int years = (int) Console.readNumber("What is the loan term length? ",
+                0, 30);
+        var calculator = new MortgageCalculator(principal, rate, years);
+        var report = new MortgageReport(calculator);
+        report.printMonthlyPayment();
+        report.printPaymentSchedule();
+
     }
 }
